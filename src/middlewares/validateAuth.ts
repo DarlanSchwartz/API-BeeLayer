738d892ce6f963ff044f86520431f7e20dfe3340
Schwartz <darlanswtz@gmail.com>
2024-05-04 00:56:45 -0300

    Feat: middleware validate auth

---
+++ src/middlewares/validateAuth.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import prisma from "../common/database/databaseConfig";
+import { CustomError, ErrorType } from "../common/protocols/error.types";
+
+export default async function validateAuth(req: Request, res: Response, next: NextFunction) {
+    const authorization = req.headers.authorization;
+    const token = authorization?.replace("Bearer ", "");
+    if (!token) throw new CustomError(ErrorType.UNAUTHORIZED, "Token not found");
+    const session = await prisma.sessions.findFirst({ where: { token } });
+    if (!session) throw new CustomError(ErrorType.UNAUTHORIZED, "Fund session not found");
+    res.locals.userId = session.user_id;
+    next();
+}
