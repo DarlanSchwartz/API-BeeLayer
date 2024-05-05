3a341080e5cf4c2a10d725e8883ea91de4b04d72
Schwartz <darlanswtz@gmail.com>
2024-05-04 21:16:13 -0300

    Fix: card service

--- src/services/card.service.ts
+++ src/services/card.service.ts
@@ -2,7 +2,6 @@ import { ethers } from "ethers";
 import { CardCheckDTO, CardDTO } from "../common/protocols/card.types";
 import { AuthenticationRepository } from "../common/repositories/bankAccount.repository";
 import { CardRepository } from "../common/repositories/card.repository";
-import { ethers } from 'ethers';
 
 const RepositoryCards = new CardRepository();
 const RepositoryUsers = new AuthenticationRepository();
